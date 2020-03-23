0b58d2fa4cff5f32a635dc0dde1e645494eef906
Brian Brady <briandbrady@mac.com>
2020-03-23 14:15:49 -0400

    Create cpy_1_03.py

---
+++ CircuitPython Assignments/cpy_1_03.py
@@ -0,0 +1,19 @@
+#
+# cpy_1_03.py
+# Light Sensor NeoPixels
+#
+# Created and tested using the VS Code CPX Device Emulator
+#
+# By: your name here
+#
+
+# import the necessary modules
+
+# write a function that maps the light level value
+# to a 0 - 1.0 value and use it to set the value
+# of the variable `bright`
+
+
+while True:
+    # place your code here for reading the level, mapping it,
+    # and setting the NeoPixel brightness
