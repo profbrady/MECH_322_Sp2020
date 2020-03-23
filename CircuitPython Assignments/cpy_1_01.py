706b0af13a899848e954af62e55e069f94b35ef3
Brian Brady <briandbrady@mac.com>
2020-03-23 14:15:42 -0400

    Create cpy_1_01.py

---
+++ CircuitPython Assignments/cpy_1_01.py
@@ -0,0 +1,16 @@
+#
+# cpy_1_01.py
+# Push Buttons and LED
+#
+# Created and tested using the VS Code CPX Device Emulator
+#
+# By: your name here
+#
+
+# import the required module(s) here
+
+while True:
+    # test for button pressing here using an `if-else` statement group
+        
+
+
