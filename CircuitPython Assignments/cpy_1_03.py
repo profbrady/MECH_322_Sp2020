35553a708bb36b54f6b716be34ef0ad4c30e488b
Brian Brady <briandbrady@mac.com>
2020-04-14 15:39:12 -0400

    Update cpy_1_03.py

--- CircuitPython Assignments/cpy_1_03.py
+++ CircuitPython Assignments/cpy_1_03.py
@@ -9,11 +9,12 @@
 
 # import the necessary modules
 
-# write a function that maps the light level value
-# to a 0 - 1.0 value and use it to set the value
-# of the variable `bright`
+# write a function named `map_level` that reads the light 
+# level value and maps it to a 0 - 1.0 value such that
+# a value of 0 correlates to a brightness of 0
 
 
 while True:
-    # place your code here for reading the level, mapping it,
-    # and setting the NeoPixel brightness
+    # place your code here for calling your function and
+    # assigning the value to the variable `bright` and then
+    # setting the NeoPixel brightness using `bright`
