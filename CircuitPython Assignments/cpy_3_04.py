9c778a5f599bebbd6152ef25d5fdb3cbe23aed7b
Brian Brady <briandbrady@mac.com>
2020-04-15 17:16:16 -0400

    Create cpy_3_04.py

---
+++ CircuitPython Assignments/cpy_3_04.py
@@ -0,0 +1,21 @@
+#
+# cpy_3_04.py
+# Simulated Motor Control with Safety Switches
+#
+# Created and tested using the VS Code CPX Device Emulator
+#
+# By: your name here
+#
+
+
+
+
+
+while True:
+    if not cp.switch:   # if switch is `False`
+        # add code for switch in the `False` position
+        
+    else:               # if switch is `True`
+        # add code for switch in the `True` position
+        
+        
