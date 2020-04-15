6de5117650c6284489a54f5682ef8a5ac059e58f
Brian Brady <briandbrady@mac.com>
2020-04-15 14:01:15 -0400

    Create cpy_3_05.py

---
+++ CircuitPython Assignments/cpy_3_05.py
@@ -0,0 +1,27 @@
+#
+# cpy_3_05.py
+# Buttons and Messages
+#
+# Created and tested using the VS Code CPX Device Emulator
+#
+# By: your name here
+#
+
+
+# import the required modules
+
+# create/set the required state tracking variable
+
+# create tuples for your colors (include the OFF color)
+
+# set NeoPixel brightness and turn off all NeoPixels
+
+
+while True:
+    if not cp.switch:   # if switch is `False`
+        # add code for switch in the `False` position
+        
+    else:               # if switch is `True`
+        # add code for switch in the `True` position
+        
+        
